868ab6630d1bf5c83dfe2975d876c5a38b83a232
Harish-365 <93084202+Harish-365@users.noreply.github.com>
2025-05-22 19:51:12 +0530

    Update 03-unit-test.sh

--- jenkins-plugin-model/ci/03-unit-test.sh
+++ jenkins-plugin-model/ci/03-unit-test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/usr/local/share/dotnet/dotnet test --logger "trx;LogFileName=Pi.Math.trx" jenkins-plugin-model/src/Pi.Math.Tests/Pi.Math.Tests.csproj
-/usr/local/share/dotnet/dotnet test --logger "trx;LogFileName=Pi.Runtime.trx" ./jenkins-plugin-model/src/Pi.Runtime.Tests/Pi.Runtime.Tests.csproj
+dotnet test --logger "trx;LogFileName=Pi.Math.trx" jenkins-plugin-model/src/Pi.Math.Tests/Pi.Math.Tests.csproj
+dotnet test --logger "trx;LogFileName=Pi.Runtime.trx" ./jenkins-plugin-model/src/Pi.Runtime.Tests/Pi.Runtime.Tests.csproj
